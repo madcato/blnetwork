192befdd8c10bbc37fb2ddb74b95e9f95aa3595b
Dani Vela <veladan@me.com>
2023-11-07 16:24:29 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# BoilerLib
+
+This package offers a set of utility functions, clases, extensions, and controls for Swift / iOS projects.
+
+Use with caution. 
+
+
---
+++ Tests/ServicesTests/MockServices/MockAPIClient.swift
@@ -0,0 +1,29 @@
+//
+//  MockAPIClient.swift
+//  iOS-BoilerplateTests
+//
+//  Created by Daniel Vela Angulo on 05/12/2018.
+//  Copyright © 2018 veladan. All rights reserved.
+//
+
+@testable import BoilerLib
+import XCTest
+
+class MockAPIClient: Http.Client {
+    struct ResponseDto: Codable {
+        var name: String
+    }
+    var objectToReturn: Http.Result<ResponseDto>?
+
+    init() {
+        super.init(baseURL: "http://www.server.com", basePath: "/path")
+    }
+
+    override func request<Response, Body>
+    (_ endpoint: Http.Endpoint<Response, Body>, completion: @escaping (Http.Result<Response>) -> Void) {
+        guard let object = objectToReturn as? Http.Result<Response> else {
+            fatalError("Need to set 'objectToReturn' value before calling this method")
+        }
+        _ = completion(object)
+    }
+}
